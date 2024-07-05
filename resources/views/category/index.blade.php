77060cc379dd909dc47d60efc97efb50841e126f
STARRED787 <malithamalshan7@gmail.com>
2024-07-05 14:49:54 +0530

    laravel

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,7 +1,34 @@
 @extends('layout')
 @extends('content')
 
+<div class="container">
+    <h3 align=" cnter">
+        Category
+    </h3>
+    </br>
 
+    <div class=" row">
+        <div class="col-md:2">
+
+        </div>
+        <div class="col-md-2">
+            <div class="frm-area">
+                <form action="({route('category.store')})">
+                    @csrf
+                    <div class="row">
+                        <label> Category Name</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
 
 
 @endsection
