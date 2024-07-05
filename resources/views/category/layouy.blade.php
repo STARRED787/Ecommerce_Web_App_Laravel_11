9f40d7643c1de52609497dcbd64b22a83a7bd65f
STARRED787 <malithamalshan7@gmail.com>
2024-07-05 14:19:37 +0530

    laravel

--- resources/views/category/layouy.blade.php
+++ resources/views/category/layouy.blade.php
@@ -6,6 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body,
+    html {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    .container {
+        display: flex;
+        height: 100%;
+    }
+
+    .sidebar {
+        flex: 0 0 250px;
+        background-color: #333;
+        color: #fff;
+    }
+
+    .sidebar ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    .sidebar ul li {
+        padding: 10px;
+    }
+
+    .sidebar ul li a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .conten {
+        flex: 1;
+        padding: 20px;
+    }
+</style>
 
 <body>
     <div class="container">
